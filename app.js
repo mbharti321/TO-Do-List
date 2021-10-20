16209f1f17cde922dbfcee7855bc947ad203e3ed
mbharti321 <mbharti321@gmail.com>
2021-10-21 00:15:20 +0530

    add work list

--- app.js
+++ app.js
@@ -6,32 +6,49 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({encoded: true}));
+app.use(bodyParser.urlencoded({ encoded: true }));
 app.use(express.static("public"))
 
 let newItems = ["buy food", "cook food", "eat", "sleep"];
+let workItems = [];
 app.get("/", function (req, res) {
 
     let today = new Date();
 
     let options = {
-        weekday:'long',
+        weekday: 'long',
         day: "numeric",
         month: "long"
     }
 
     let day = today.toLocaleDateString("en-US", options);
-   
+
     res.render("list", {
-        kindOfDay: day,
+        listTitle: day,
         newListItems: newItems
     });
 });
-
 app.post("/", function (req, res) {
-    newItems.push( req.body.newItem);
-    res.redirect("/")
+    if (req.body.listBtn === "work") {
+        workItems.push(req.body.newItem);
+        res.redirect("/work")
+    } else {
+        newItems.push(req.body.newItem);
+        res.redirect("/")
+    }
+
 });
+
+
+app.get("/work", function (req, res) {
+    let options = {
+        listTitle: "work",
+        newListItems: workItems
+    }
+    res.render("list", options)
+});
+
+
 
 app.listen(3000, function () {
     console.log("Server Started!!\nListening to port: http://localhost:3000/")
